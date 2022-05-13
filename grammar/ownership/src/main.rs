a562b9d0c2aa4d9e0b77dceb013e93e01e79ec51
jweboy <1259524459@qq.com>
2022-05-13 13:08:58 +0800

    feat(ownership): update learn demo

--- grammar/ownership/src/main.rs
+++ grammar/ownership/src/main.rs
@@ -32,4 +32,38 @@ fn main() {
     let x = 5;
     let y = x;
     println!("{}, {}", x, y);
+
+    let s = String::from("Hello, world!");
+    take_ownership(s);
+
+    let x = 15;
+    makes_copy(x);
+    println!("{}", x);
+
+    {
+        // 函数返回值过程中也会发生所有权转移
+        let a1 = gives_ownership(); // some_thing 所有权移动给了 a1
+        let a2 = String::from("Hello");
+        let a3 = takes_and_gives_back(a2); // a_string 所有权移动给了 a3
+
+        fn gives_ownership() -> String {
+            let some_thing = String::from("Hello");
+            some_thing
+        }
+
+        fn takes_and_gives_back(a_string: String) -> String {
+            a_string
+        }
+    } // a1、a3 离开了作用域被销毁，a2所有权被移动了，在这里是安全的
+
+    // 变量所有权在赋值过程中会发生移动
+    // 当一个包含 heap 数据的变量离开作用域，它的值会被 drop 函数清理，除非所有权移动到了另一个变量上
+}
+
+fn take_ownership(some_thing: String) {
+    println!("{}", some_thing);
+}
+
+fn makes_copy(some_number: i32) {
+    println!("{}", some_number);
 }
